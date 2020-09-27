382663419cfd2cfb2bc243af2ae348a358a35ebd
Valentin Terebenin <terebenin.work@gmail.com>
2020-09-27 13:08:55 +0300

    initial: created simple logic for counter, testing composition api

---
+++ src/components/Counter.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+button(@click="addMoreOdobryams")
+  | count is: {{ state.count  }}
+</template>
+<script>
+import { reactive, computed } from 'vue'
+export default {
+  setup () {
+    const state = reactive({
+      count: 0,
+      double: computed(() => state.count * 2)
+    })
+
+    const addMoreOdobryams = () => {
+      state.count++
+    }
+
+    return {
+      state,
+      addMoreOdobryams
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+img(src="@/assets/theCat.png")
+h2 The one and only:
+h1 Odobryams!
+counter
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Counter from '@/components/Counter'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: { Counter }
 }
 </script>
